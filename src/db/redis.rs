2ce290f45ef68bf446d80168a22a3ab643422b93
Pavlo Mitin <mitin.pavel@gmail.com>
2017-10-28 10:34:56 +0100

    Refactor "main" module extracting redis related stuff to "redis" module

--- src/db/redis.rs
+++ src/db/redis.rs
@@ -5,10 +5,31 @@ use rocket::State;
 use r2d2;
 use r2d2_redis::RedisConnectionManager;
 
-use Pool;
+const REDIS_ADDRESS: &'static str = "redis://localhost:6379";
 
+// Pool initiation.
+// Call it starting an app and store a pul as a rocket managed state.
+pub fn pool() -> Pool {
+    let manager = RedisConnectionManager::new(REDIS_ADDRESS).expect("connection manager");
+    let redis_config = Default::default();
+
+    r2d2::Pool::new(redis_config, manager).expect("db pool")
+}
+
+// Rocket guard type: a wrapper around an r2d2 pool.
+// In conjunction with
+// `impl<'a, 'r> request::FromRequest<'a, 'r> for RedisConnection` (see below)
+// it allows code like:
+//   ```
+//   #[post("/<item>")]
+//   fn create(item: &RawStr, connection: RedisConnection) -> ...
+//
 pub struct RedisConnection(pub r2d2::PooledConnection<RedisConnectionManager>);
 
+// An alias to the type for a pool of redis connections.
+type Pool = r2d2::Pool<RedisConnectionManager>;
+
+// Retrieving a single connection from the managed database pool.
 impl<'a, 'r> request::FromRequest<'a, 'r> for RedisConnection {
     type Error = ();
 
